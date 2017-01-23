fd7a97035ebaf64eb05fd91d2169a8bfe6fd22c5
Talbot <talbotlawrence75@gmail.com>
2017-01-23 15:34:48 -0600

    got the submit button to work

--- strings.js
+++ strings.js
@@ -1,19 +1,43 @@
-function reversal() {
+// function validateText(text) {
+// 	var getClass = document.getElementsByClassName("textVal")[0].value;
+// 	console.log("getClass", getClass);
+// 	var textRegex = /[A-Z][a-z]/;
+// 	var testInput = getClass.search(textRegex);
+// }
+console.log("hi");
+var userInput;
 
 
-}
-
-function alphabits() {
-
-
-}
-
-function palindrome() {
+document.getElementById("pleaseSubmit")
+    .addEventListener("click", function(event){
+     userInput = document.getElementById("textVal").value;
+     console.log("userInput", userInput);
+});
 
 
-}
 
-var testString = "";
-reversal(testString);
-alphabits(testString);
-palindrome(testString);
+/////////////////////////////////////////////////////////////////////////////////////
+// function reversal(anything) {
+// 	//reverses the order of the characters in the string, 
+// 	//and outputs the result in the DOM, below the text input.
+
+// 	var testString = anything.target.innerHTML;
+// 	return testString;
+// }
+
+// function alphabits() {
+// 	//return the characters in alphabetical order, 
+// 	//and outputs the result in the DOM, below the text input.
+
+// }
+
+// function palindrome() {
+// 	//determine whether the string is a palindrome. 
+// 	//If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
+
+// }
+
+// var testString = "";
+// reversal(testString);
+// alphabits(testString);
+// palindrome(testString);
