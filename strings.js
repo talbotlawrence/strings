4b737bb322ae994740d60c8693bbc3e81f891369
Talbot <talbotlawrence75@gmail.com>
2017-01-23 14:08:02 -0600

    first commit

---
+++ strings.js
@@ -0,0 +1,19 @@
+function reversal() {
+
+
+}
+
+function alphabits() {
+
+
+}
+
+function palindrome() {
+
+
+}
+
+var testString = "";
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
