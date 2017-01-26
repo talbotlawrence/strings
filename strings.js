368954b5f865d4e952e7a5959b03acfeb2fd2dec
Talbot <talbotlawrence75@gmail.com>
2017-01-25 20:42:19 -0600

    nothing works

--- strings.js
+++ strings.js
@@ -1,66 +1,71 @@
-console.log("hi");
+//console.log("hi");
 
-//MAKE MORE FUNCTIONS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+var finalResult = document.getElementById("dumpIt");            //Global variables ugh!
+console.log("finalResult", finalResult);
+var testString = document.getElementById("textVal").value;       //I need to get better with IIFEs
+console.log("testString", testString);
+var submitButton = document.getElementById("pleaseSubmit");
 
-var userInput;
+if (testString === "") {	//this is for testing purposes.
+	testString = "Talbot";
+};
 
-function getUserInput() {
-
-}
-	document.getElementById("pleaseSubmit")
-    .addEventListener("click", function(event){
-     userInput = document.getElementById("textVal").value;
-	     
-     console.log("userInput", userInput);
-})
-
-
-if (isNaN(parseInt(userInput))) {
-			alert("Please do not enter numbers");
-			userInput = "";
-}
-
+submitButton.addEventListener("click", myRegex);
+console.log("after regex addEventListener", testString);
+submitButton.addEventListener("click", reversal);
+submitButton.addEventListener("click", alphabits);
+submitButton.addEventListener("click", palindrome);
+////////////////////////////////////////////////////////////////////////////////////////
+function myRegex() {
+    console.log("testString in Regex", testString)
+    var letters = /^[A-Za-z]+$/;
+    if (testString.match(letters)) {
+        alert("Please do not enter numbers.  It makes Talbot very unhappy!!");
+        window.location.reload();
+        break;
+    }
+};
 
 //Enter button will submit
-document.getElementById("textVal").addEventListener("keyup", function(e) {
+testString.addEventListener("keyup", function(e) {
     if (e.keyCode == 13) {
-        document.getElementById("pleaseSubmit").click();
+        submitButton.click();
     }   
 });
 
-
-// var textRegex = /[0-9]/;
-// if (textRegex.test(userInput)) {
-// 	alert("Please do not enter numbers");
-// 	window.location.reload();
-	break;
-// }
-
-
-
-
 /////////////////////////////////////////////////////////////////////////////////////
-// function reversal(anything) {
-// 	//reverses the order of the characters in the string, 
-// 	//and outputs the result in the DOM, below the text input.
 
-// 	var testString = anything.target.innerHTML;
-// 	return testString;
-// }
+function reversal(userInput) {
+	//reverses the order of the characters in the string, 
+	//and outputs the result in the DOM, below the text input.
+    
+    finalResult.innerHTML = userInput.split("").reverse().join("") + " reversal";	
+};
 
-// function alphabits() {
-// 	//return the characters in alphabetical order, 
-// 	//and outputs the result in the DOM, below the text input.
+function alphabits(userInput) {
+	//return the characters in alphabetical order, 
+	//and outputs the result in the DOM, below the text input.
+    
+    finalResult.innerHTML = userInput.split("").sort().join("") + " alphabits";
+};
 
-// }
 
-// function palindrome() {
-// 	//determine whether the string is a palindrome. 
-// 	//If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
+function palindrome(userInput) {
+	//determine whether the string is a palindrome. 
+	//If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
+    
+    var reverseUserInput = userInput.split("").reverse().join("")
+    
+    if (userInput === "") {
+        finalResult.innerHTML = "";
+    } else if (userInput === reverseUserInput) {
+        finalResult.innerHTML = userInput + " palindrome";
+    } else {
+        finalResult.innerHTML = userInput + " ---no palindrome for you!";
+    }
 
-// }
+};
 
-// var testString = "";
-// reversal(testString);
-// alphabits(testString);
-// palindrome(testString);
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
