019e13b55ff3ff422172de892ec6d85b9aeefa83
Talbot <talbotlawrence75@gmail.com>
2017-01-29 18:06:03 -0600

    added more to strings.js

--- strings.js
+++ strings.js
@@ -1,69 +1,66 @@
-//console.log("hi");
+//I hate functions only because they hate me!!!!!!!!!!!
+//I need to get better with IIFEs
+//Global variables ugh!
 
-var finalResult = document.getElementById("dumpIt");            //Global variables ugh!
-console.log("finalResult", finalResult);
-var testString = document.getElementById("textVal").value;       //I need to get better with IIFEs
-console.log("testString", testString);
-var submitButton = document.getElementById("pleaseSubmit");
-
-if (testString === "") {	//this is for testing purposes.
-	testString = "Talbot";
+var testString = document.getElementById("input").value; 
+if (testString === undefined) {
+	testString = "Hello";
 };
+var submitButton = document.getElementById("submit");
+var finalResult = document.getElementById("showMe");  			
 
 submitButton.addEventListener("click", myRegex);
-console.log("after regex addEventListener", testString);
 submitButton.addEventListener("click", reversal);
 submitButton.addEventListener("click", alphabits);
 submitButton.addEventListener("click", palindrome);
-////////////////////////////////////////////////////////////////////////////////////////
+
 function myRegex() {
-    console.log("testString in Regex", testString)
-    var letters = /^[A-Za-z]+$/;
-    if (testString.match(letters)) {
-        alert("Please do not enter numbers.  It makes Talbot very unhappy!!");
-        window.location.reload();
-        break;
-    }
+	var letters = /^[A-Za-z]+$/;
+	if (testString.match(letters)) {
+		alert("Please do not enter numbers.  It makes Talbot very unhappy!!");
+		window.location.reload();
+		break;
+	}
 };
 
 //Enter button will submit
-testString.addEventListener("keyup", function(e) {
-    if (e.keyCode == 13) {
-        submitButton.click();
-    }   
-});
+testString.onkeypress = function(e){
+    if(e.keyCode === 13) {
+        //e.preventDefault();
+        var pressedEnter = submitButton.click();
+    }
+}
 
-/////////////////////////////////////////////////////////////////////////////////////
-
-function reversal(userInput) {
+function reversal(testString) {
 	//reverses the order of the characters in the string, 
 	//and outputs the result in the DOM, below the text input.
-    
-    finalResult.innerHTML = userInput.split("").reverse().join("") + " reversal";	
-};
 
-function alphabits(userInput) {
+	var myReversal = testString.split("").reverse().join("") + " reversal";	
+	console.log("myReversal is: ", myReversal);
+	//finalResult.innerHTML = ``
+};
+function alphabits(testString) {
 	//return the characters in alphabetical order, 
 	//and outputs the result in the DOM, below the text input.
-    
-    finalResult.innerHTML = userInput.split("").sort().join("") + " alphabits";
+
+	var myAlphabits = testString.split("").sort().join("") + " alphabits";
+	console.log("myAlphabits are: ", myAlphabits);
+	//finalResult.innerHTML = `` 
 };
-
-
-function palindrome(userInput) {
+function palindrome(testString) {
 	//determine whether the string is a palindrome. 
 	//If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
-    
-    var reverseUserInput = userInput.split("").reverse().join("")
-    
-    if (userInput === "") {
-        finalResult.innerHTML = "";
-    } else if (userInput === reverseUserInput) {
-        finalResult.innerHTML = userInput + " palindrome";
-    } else {
-        finalResult.innerHTML = userInput + " ---no palindrome for you!";
-    }
 
+	var myPalindrome = testString.split("").reverse().join("")
+	console.log("myPalindrome is: ", myPalindrome);
+
+		if (testString === "") {
+			finalResult.innerHTML = "";
+		} else if (testString === myPalindrome) {
+			finalResult.innerHTML = `${testString} palindrome`;
+		} else {
+			finalResult.innerHTML = `${testString}---no palindrome for you!`;
+		}
 };
 
 reversal(testString);
